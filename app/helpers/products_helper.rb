a8fdd02fe54a414d986fc4d78b67b4b0eab0f5ad
Maksim Karganov <maksim.karganov@gmail.com>
2014-01-12 11:50:43 +0200

    layout changes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,8 @@ module ProductsHelper
   def total_price(products)
     products.map(&:price).inject {|price, total| total + price }
   end
+
+  def price_with_currency(price)
+    "#{price} " << t('shared.product.currency')
+  end
 end

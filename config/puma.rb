1c72f31f21fc7f4d32ac545b1f33650ec7932ce9
Maksim Karganov <maksim.karganov@gmail.com>
2014-03-12 15:30:00 +0200

    change directories for server

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,8 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 threads 4,4
 
-bind  "unix:///data/apps/appname/shared/tmp/puma/appname-puma.sock"
-pidfile "/data/apps/appname/current/tmp/puma/pid"
-state_path "/data/apps/appname/current/tmp/puma/state"
+bind  "unix:///home/deploy/shared/server/aqualuks-puma.sock"
+pidfile "/home/deploy/shared/server/puma/pid"
+state_path "/home/deploy/shared/server/state"
 
 activate_control_app

84bc0bdba3ffdb27352fa3a68b88bc0ad0c90e32
Maksim Karganov <maksim.karganov@gmail.com>
2014-03-12 15:53:55 +0200

    remove old javascripts from old bootstrap, fix image css links

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 threads 4,4
 
 bind  "unix:///home/deploy/shared/server/aqualuks-puma.sock"
-pidfile "/home/deploy/shared/server/puma/pid"
+pidfile "/home/deploy/shared/server/pid"
 state_path "/home/deploy/shared/server/state"
 
 activate_control_app
